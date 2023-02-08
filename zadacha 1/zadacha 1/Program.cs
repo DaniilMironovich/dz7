a1d6ce47c01fd442fe3973bb4a6b31089adf562c
Danik <l13m@mail.ru>
2023-02-08 19:25:56 +0300

    1

---
+++ zadacha 1/zadacha 1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+using System.Globalization;
+
+double[,] array = new double[3, 4];
+Random rand = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = 1+ rand.NextDouble() * (10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
